a47e2d62d0f1f17859a4b00eb8a551deb24f15f2
JRBotta <jeremy.r.botta@gmail.com>
2018-07-30 15:55:49 -0600

    spring stubbed

---
+++ FinalREST/src/main/java/com/skilldistillery/dmtool/FinalRestApplication.java
@@ -0,0 +1,12 @@
+package com.skilldistillery.dmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalRestApplication.class, args);
+	}
+}
