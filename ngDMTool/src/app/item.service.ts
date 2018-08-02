b33c00d4aabdfe0bfe874b26e7f63db3155c46d9
jmgroves <jared.m.groves@gmail.com>
2018-08-02 14:10:02 -0600

    fix

--- ngDMTool/src/app/item.service.ts
+++ ngDMTool/src/app/item.service.ts
@@ -38,7 +38,7 @@ export class ItemService {
 
   update(uid, iid, item) {
     console.log(`${this.url}user/${uid}/item/${iid}`);
-    return this.http.patch<CampaignNote>(`${this.url}user/${uid}/item/${iid}`, item).pipe(
+    return this.http.patch<Item>(`${this.url}user/${uid}/item/${iid}`, item).pipe(
       catchError((err: any) => {
         console.log(err);
         return throwError('KABOOM');
