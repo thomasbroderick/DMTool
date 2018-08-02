1b7436a110a0b4083d37ea31b6675c2bf6fd9b53
Tom Broderick <tombroderick@gmail.com>
2018-08-02 11:56:47 -0600

    populated monster table

--- db/jsonConverter.js
+++ db/jsonConverter.js
@@ -1,6 +1,43 @@
 const fs = require('fs');
+var stringify = require('json-stable-stringify');
 
-fs.readFile('monsters_small.json', (err, data) => {
+
+var requiredProperties = [
+"user_id",
+"name",
+"size",
+"type",
+"subtype",
+"alignment",
+"armor_class",
+"hit_points",
+"hit_dice",
+"speed",
+"strength",
+"dexterity",
+"intelligence",
+"wisdom",
+"charisma",
+"constitution",
+"dexterity_save",
+"charisma_save",
+"wisdom_save",
+"stealth",
+"damage_vulnerabilities",
+"damage_resistances",
+"damage_immunities",
+"condition_immunities",
+"senses",
+"languages",
+"challenge_rating",
+"special_abilities",
+"actions",
+"legendary_actions",
+"image_url"
+];
+
+
+fs.readFile('monsters.json', (err, data) => {
     if (err) throw err;
     let arrayOfObjects = JSON.parse(data);
     //console.log(arrayOfObjects);
@@ -15,13 +52,14 @@ function fix(input) {
       for (var i=0; i < monster.special_abilities.length; i++) {
         var ability = monster.special_abilities[i];
         for (var p in ability) {
-          saString += p + " : " + ability[p] + "\n";
+          saString += p + " : " + ability[p] + "---";
         }
         if ( i < monster.special_abilities.length -1 ) {
-          saString += "================================\n";
+          saString += "================================";
         }
       }
     }
+    monster.user_id = 1;
     monster.special_abilities = saString;
 
     var actString = "";
@@ -29,10 +67,10 @@ function fix(input) {
       for (var i=0; i < monster.actions.length; i++) {
         var action = monster.actions[i];
         for (var p in action) {
-          actString += p + " : " + action[p] + "\n";
+          actString += p + " : " + action[p] + "---";
         }
         if ( i < monster.actions.length -1 ) {
-          actString += "================================\n";
+          actString += "================================";
         }
       }
     }
@@ -43,18 +81,40 @@ function fix(input) {
       for (var i=0; i < monster.legendary_actions.length; i++) {
         var legAction = monster.legendary_actions[i];
         for (var p in legAction) {
-          legActString += p + " : " + legAction[p] + "\n";
+          legActString += p + " : " + legAction[p] + "---";
         }
         if ( i < monster.legendary_actions.length -1 ) {
-          legActString += "================================\n";
+          legActString += "================================";
         }
       }
     }
     monster.legendary_actions = legActString;
 
+    delete monster.history;
+    monster.image_url = monster.url;
+    delete monster.url;
+    delete monster.index;
+    delete monster.perception;
+    delete monster.constitution_save;
+    monster.challenge_rating = "CR: " + monster.challenge_rating;
+
+    requiredProperties.forEach(function(prop){
+      if ( monster[prop] == undefined ) {
+        monster[prop] = null;
+      }
+    });
+
+    for ( p in monster ) {
+      if ( ! requiredProperties.includes(p)) {
+        delete monster[p];
+      }
+    }
 
   });
-  var json = JSON.stringify(input);
+  //var json = JSON.stringify(input);
+  //var json = JSON.stringify(input, Object.keys(input).sort());
+  var json = stringify(input);
+
   return json;
 }
 
