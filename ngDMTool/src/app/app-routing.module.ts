15835890422075a1bfd34512942a3763a62b24e6
Tom Broderick <tombroderick@gmail.com>
2018-08-03 16:19:40 -0600

    updated users in db

--- ngDMTool/src/app/app-routing.module.ts
+++ ngDMTool/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
 import { AuthenticationComponent } from './authentication/authentication.component';
+import { LibraryComponent } from './library/library.component';
 
 const routes: Routes = [
   { path: 'admin', component: AdminComponent },
-  { path: 'login', component: AuthenticationComponent }
+  { path: 'login', component: AuthenticationComponent },
+  { path: 'library', component: LibraryComponent }
 ];
 
 @NgModule({
