bec2eb53605dbd2ff756a4b65683d55dd6894038
jmgroves <jared.m.groves@gmail.com>
2018-08-09 10:11:07 -0600

    security aws

--- FinalJPA/src/com/skilldistillery/dmtool/entities/Monster.java
+++ FinalJPA/src/com/skilldistillery/dmtool/entities/Monster.java
@@ -12,7 +12,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Transient;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Monster {
--- FinalREST/src/main/java/com/skilldistillery/dmtool/services/AuthServiceImpl.java
+++ FinalREST/src/main/java/com/skilldistillery/dmtool/services/AuthServiceImpl.java
@@ -2,11 +2,9 @@ package com.skilldistillery.dmtool.services;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
