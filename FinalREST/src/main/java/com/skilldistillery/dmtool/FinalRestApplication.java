d1f1f1b59cea9ddae33073f6f1ad3d736b5cbd94
JRBotta <jeremy.r.botta@gmail.com>
2018-08-09 10:04:10 -0600

    WAR

--- FinalREST/src/main/java/com/skilldistillery/dmtool/FinalRestApplication.java
+++ FinalREST/src/main/java/com/skilldistillery/dmtool/FinalRestApplication.java
@@ -2,12 +2,14 @@ package com.skilldistillery.dmtool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class FinalRestApplication {
+public class FinalRestApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FinalRestApplication.class, args);
@@ -16,4 +18,10 @@ public class FinalRestApplication {
 	public PasswordEncoder configurePasswordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
+	
+	@Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	      return application.sources(FinalRestApplication.class);
+	  }
+
 }
